585371fb8a6b6f9392935d05499a234d069262d0
toogley <toogley@mailbox.org>
2017-02-27 23:23:10 +0100

    imap: add net_folder_format

--- globals.h
+++ globals.h
@@ -69,6 +69,7 @@ WHERE char *Escape;
 WHERE char *FolderFormat;
 WHERE char *ForwardAttributionIntro;
 WHERE char *ForwardAttributionTrailer;
+WHERE char *NetFolderFormat;
 WHERE char *ForwardFormat;
 WHERE char *Hostname;
 WHERE struct MbTable *FromChars;
