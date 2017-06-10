f03d8cc6937d8a9d154a81657810eb537e97625b
toogley <toogley@mailbox.org>
2017-06-10 22:25:35 +0200

    make +DEBUG persistent

--- globals.h
+++ globals.h
@@ -239,10 +239,8 @@ WHERE short NntpPoll;
 WHERE short NntpContext;
 #endif
 
-#ifdef DEBUG
 WHERE short DebugLevel;
 WHERE char *DebugFile;
-#endif
 
 WHERE short ConnectTimeout;
 WHERE short History;
